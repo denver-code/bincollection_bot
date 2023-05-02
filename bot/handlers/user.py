760c774bbc6ea3be6fbb77189e9fc3e3b1849e2d
denver-code <csigorek@gmail.com>
2023-05-02 20:01:59 +0100

    set_user_location_event -> controllers

--- bot/handlers/user.py
+++ bot/handlers/user.py
@@ -1,29 +1,6 @@
-from aiogram import Dispatcher, types, dispatcher
-
-from core.models.user import Location as LocationModel, User as UserModel
-from core.redis import is_user_exist, set_user, get_user
+from aiogram import Dispatcher
+from core.controllers.user import set_user_location_event
 from core.states.location import LocationFetcher
-
-async def set_user_location_event(message: types.Message, state: dispatcher.FSMContext):
-    if not message.text.isdigit():
-        return await message.answer("Looks like your UPRN number are invalid. Try again!")
-    
-    _location = LocationModel(UPRN=message.text)
-    
-    if not is_user_exist(message.from_id):
-        _user = UserModel(
-            id=message.from_id,
-            is_subscribed=False,
-            location=_location
-        )
-        set_user(message.from_id, _user.to_json())
-        return await message.answer("Welcome in our team! Now you able to use every function. Type /menu to check them out!")
-
-    _user = get_user(message.from_id)
-    _user["location"] = _location
-
-    set_user(message.from_id, _user)
-    await message.answer("Location updated successfully!")
 
 
 def setup(dp: Dispatcher):
---
+++ core/controllers/user.py
@@ -0,0 +1,26 @@
+from aiogram import types, dispatcher
+
+from core.models.user import Location as LocationModel, User as UserModel
+from core.redis import is_user_exist, set_user, get_user
+
+async def set_user_location_event(message: types.Message, state: dispatcher.FSMContext):
+    await state.finish()
+    if not message.text.isdigit():
+        return await message.answer("Looks like your UPRN number are invalid. Try again!")
+    
+    _location = LocationModel(UPRN=message.text)
+    
+    if not is_user_exist(message.from_id):
+        _user = UserModel(
+            id=message.from_id,
+            is_subscribed=False,
+            location=_location
+        )
+        set_user(message.from_id, _user.to_json())
+        return await message.answer("Welcome in our team! Now you able to use every function. Type /menu to check them out!")
+
+    _user = get_user(message.from_id)
+    _user["location"] = _location
+
+    set_user(message.from_id, _user)
+    await message.answer("Location updated successfully!")
